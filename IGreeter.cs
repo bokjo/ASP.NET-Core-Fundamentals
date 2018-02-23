09c7df40c67d7bba1337c28a3c10fbdb0d33233f
bokjo <bstojcevski89@gmail.com>
2018-02-23 15:23:19 +0100

    Created confg file,implemented,registered and injected my custom service

---
+++ IGreeter.cs
@@ -0,0 +1,23 @@
+using Microsoft.Extensions.Configuration;
+
+namespace ASP.NET_Core_Fundamentals
+{
+    public interface IGreeter
+    {
+        string GetMessageOfTheDay();
+    }
+
+    public class Greeter : IGreeter
+    {
+        private IConfiguration _config;
+
+        public Greeter(IConfiguration config)
+        {
+            _config = config;
+        }
+        public string GetMessageOfTheDay()
+        {
+            return _config["Greeting"];
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace ASP.NET_Core_Fundamentals
@@ -15,10 +16,11 @@ namespace ASP.NET_Core_Fundamentals
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<IGreeter, Greeter>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IGreeter greeter)
         {
             if (env.IsDevelopment())
             {
@@ -27,7 +29,8 @@ namespace ASP.NET_Core_Fundamentals
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync(text: "Hello World from Bokjo!");
+                var greeting = greeter.GetMessageOfTheDay();
+                await context.Response.WriteAsync(greeting);
             });
         }
     }
