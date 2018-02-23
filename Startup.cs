938997187061d7c7e6094d7070cda389bafd87fe
bokjo <bstojcevski89@gmail.com>
2018-02-23 17:41:35 +0100

    Explored the middleware,  custom delegate and served static files

--- Startup.cs
+++ Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace ASP.NET_Core_Fundamentals
 {
@@ -20,15 +21,42 @@ namespace ASP.NET_Core_Fundamentals
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IGreeter greeter)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IGreeter greeter, ILogger<Startup> logger)
         {
             if (env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
+                 app.UseDeveloperExceptionPage(); 
             }
+
+            // app.Use( next =>
+            // {
+            //     return async context => {
+            //         logger.LogInformation("Request incoming!");
+            //         if (context.Request.Path.StartsWithSegments("/mym"))
+            //         {
+            //             await context.Response.WriteAsync("Hit!!!");
+            //             logger.LogInformation("Request handled!!!");        
+            //         }
+            //         else 
+            //         {
+            //             await next(context);
+            //             logger.LogInformation("Response outgoing!!!");    
+            //         }
+            //     };
+            // });
+
+            // app.UseWelcomePage(new WelcomePageOptions {
+            //     Path="/welcome"
+            // });
+
+            // app.UseFileServer();
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
 
             app.Run(async (context) =>
             {
+                // throw new Exception("Unexpected error occurred! XA");   // HC exeption for testing the "DeveloperExceptionPage()"
+ 
                 var greeting = greeter.GetMessageOfTheDay();
                 await context.Response.WriteAsync(greeting);
             });
