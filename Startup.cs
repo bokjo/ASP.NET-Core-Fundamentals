df907a9ed3fc427c9dcc06dbea6be72796538c48
bokjo <bstojcevski89@gmail.com>
2018-03-02 16:41:58 +0100

    Added Bootstrap4 and styling, jQuery, client side validations,  serving files from node_modules and CDN with fallbacks...

--- Startup.cs
+++ Startup.cs
@@ -1,14 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using ASP.NET_Core_Fundamentals.Data;
+﻿using ASP.NET_Core_Fundamentals.Data;
 using ASP.NET_Core_Fundamentals.Services;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Rewrite;
 using Microsoft.AspNetCore.Routing;
 using Microsoft.EntityFrameworkCore;
@@ -26,8 +21,7 @@ namespace ASP.NET_Core_Fundamentals
         {
             _config = config;
         }
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddAuthentication(options =>
@@ -43,66 +37,32 @@ namespace ASP.NET_Core_Fundamentals
 
             services.AddSingleton<IGreeter, Greeter>();
             services.AddDbContext<DataDbContext>(options => options.UseSqlServer(_config.GetConnectionString("OdeToFood")));
-
-            //services.AddScoped<IRestaurantData, MockupRestaurantData>();
             services.AddScoped<IRestaurantData, DbRestaurantData>();
             services.AddMvc();
         }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, IGreeter greeter, ILogger<Startup> logger)
         {
             if (env.IsDevelopment())
             {
                  app.UseDeveloperExceptionPage(); 
             }
-
-            // app.Use( next =>
-            // {
-            //     return async context => {
-            //         logger.LogInformation("Request incoming!");
-            //         if (context.Request.Path.StartsWithSegments("/mym"))
-            //         {
-            //             await context.Response.WriteAsync("Hit!!!");
-            //             logger.LogInformation("Request handled!!!");        
-            //         }
-            //         else 
-            //         {
-            //             await next(context);
-            //             logger.LogInformation("Response outgoing!!!");    
-            //         }
-            //     };
-            // });
-
-            // app.UseWelcomePage(new WelcomePageOptions {
-            //     Path="/welcome"
-            // });
-
+     
             app.UseRewriter(new RewriteOptions().AddRedirectToHttpsPermanent());
 
             app.UseStaticFiles();
 
-            app.UseAuthentication();
+            app.UseNodeModules(env.ContentRootPath);
 
-            //app.UseMvcWithDefaultRoute();
+            app.UseAuthentication();
+            
             app.UseMvc(configureRoutes);
 
-
-            app.Run(async (context) =>
-            {
-                // throw new Exception("Unexpected error occurred! XA");   // HC exeption for testing the "DeveloperExceptionPage()"
- 
-                var greeting = greeter.GetMessageOfTheDay();
-                await context.Response.WriteAsync(greeting);
-
-            });
         }
 
         private void configureRoutes(IRouteBuilder routeBuilder)
         {
-            // /Home/Index
-            routeBuilder.MapRoute("Default",  
-                                  "{controller=Home}/{action=Index}/{id?}");
+            routeBuilder.MapRoute("Default", "{controller=Home}/{action=Index}/{id?}");
         }
     }
 }
